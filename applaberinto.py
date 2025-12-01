b90f6ebac26e9d0a73d48e66831be336daa5378c
HumanAgonyKys <164834996+HumanAgonyKys@users.noreply.github.com>
2025-12-01 17:48:56 -0600

    Refactor BFS maze solver and update maze definition
    Refactor maze solving algorithm to improve readability and efficiency.

--- applaberinto.py
+++ applaberinto.py
@@ -1,8 +1,28 @@
 import collections
 
+# Configuración del laberinto
+# 0: camino libre, 1: muro
+MAZE = [
+    [0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 0, 1, 0, 0, 0]
+]
+
+START = (0, 0)
+END = (9, 9)
+
 def solve_maze_bfs(maze, start, end):
-    """Resuelve el laberinto usando el algoritmo de BÃºsqueda en Amplitud (BFS)."""
+    """Resuelve el laberinto usando BFS y retorna el camino."""
     rows, cols = len(maze), len(maze[0])
+    
+    # Cola almacena: (coordenada_actual, camino_hasta_aqui)
     queue = collections.deque([(start, [start])])
     visited = set()
     visited.add(start)
@@ -13,49 +33,16 @@ def solve_maze_bfs(maze, start, end):
         if (curr_row, curr_col) == end:
             return path
 
-        # Movimientos posibles: arriba, abajo, izquierda, derecha
-        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        # Movimientos: Arriba, Abajo, Izquierda, Derecha
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             next_row, next_col = curr_row + dr, curr_col + dc
-            
-            if 0 <= next_row < rows and 0 <= next_col < cols and \
-               maze[next_row][next_col] == 0 and (next_row, next_col) not in visited:
-                visited.add((next_row, next_col))
-                new_path = list(path)
-                new_path.append((next_row, next_col))
-                queue.append(((next_row, next_col), new_path))
+
+            # Verificamos límites del mapa
+            if 0 <= next_row < rows and 0 <= next_col < cols:
+                # Verificamos que no sea muro (1) y no haya sido visitado
+                if maze[next_row][next_col] == 0 and (next_row, next_col) not in visited:
+                    visited.add((next_row, next_col))
+                    new_path = path + [(next_row, next_col)] # Crea una nueva lista con el nuevo paso
+                    queue.append(((next_row, next_col), new_path))
     
-    return None # No se encontrÃ³ camino
-
-# Puedes implementar DFS de manera similar, usando una lista como pila (o recursiÃ³n).
-
-# RepresentaciÃ³n del laberinto (0: camino libre, 1: muro, 2: inicio, 3: fin)
-# Un laberinto de ejemplo:
-
-MAZE = [
-        [0,1,0,0,0,0,1,0,0,0],
-        [0,1,0,1,1,0,1,0,1,0],
-        [0,0,0,0,1,0,0,0,1,0],
-        [1,1,1,0,1,1,1,0,1,0],
-        [0,0,0,0,0,0,0,0,1,0],
-        [0,1,1,1,1,1,1,0,1,0],
-        [0,0,0,0,0,0,1,0,1,0],
-        [0,1,1,1,1,0,1,0,1,0],
-        [0,0,0,0,1,0,0,0,1,0],
-        [0,1,1,0,0,0,1,0,0,0]
-    ]
-
-#MAZE = [
-#    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-#    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-#    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
-#    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-#    [1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-#    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-#    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-#]
-
-START = (0, 0)
-END = (9, 9)
+    return None # No se encontró camino
